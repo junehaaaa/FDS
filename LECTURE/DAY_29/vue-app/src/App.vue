cf688e1580018edd7aea7716a85f82c5ae2c555d
Juneha Jang <junehaaaa@gmail.com>
2017-03-28 09:24:42 +0900

    오류 수정

--- LECTURE/DAY_29/vue-app/src/App.vue
+++ LECTURE/DAY_29/vue-app/src/App.vue
@@ -1,27 +1,13 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
-    <img src="./assets/logo.png">
-    <h1>Hello Vue</h1>
-    <h2>Essential Links</h2>
-=======
     <headers>
 
     </headers>
->>>>>>> b1f20162b0aed533649249b06419243ea3ff8c38
   </div>
 </template>
 
 <script>
-<<<<<<< HEAD
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-=======
 import Headers from './components/Headers.vue';
-
 export default {
   name: 'app',
   data() {
@@ -35,46 +21,15 @@ export default {
   computed: {
     memory() {
       return 10;
->>>>>>> b1f20162b0aed533649249b06419243ea3ff8c38
     }
   }
 }
 </script>
 
-<<<<<<< HEAD
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-=======
 <style lang="sass" scoped>
   .container
     width: 800px
     margin:
       left: auto
       right: auto
->>>>>>> b1f20162b0aed533649249b06419243ea3ff8c38
 </style>
